b8d52631860799b5a4b6ab8cfb974df9e39d0630
Abilat01 <abilat01@yandex.ru>
2020-08-04 11:40:18 +0300

    Fragment listener

--- app/src/main/java/com/abilat01/steelweightv2/MainActivity.kt
+++ app/src/main/java/com/abilat01/steelweightv2/MainActivity.kt
@@ -2,10 +2,49 @@ package com.abilat01.steelweightv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Загрузка первых фрагментов
+        loadFragment(ExploreFragment())
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when {
+                menuItem.itemId == R.id.navigationExplore -> {
+                    loadFragment(ExploreFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                menuItem.itemId == R.id.navigationDashboard -> {
+                    loadFragment(DashboardFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                menuItem.itemId == R.id.navigationProfile ->{
+                    loadFragment(ProfaleFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+
+                }
+            }
+
+        }
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().also { fragmentTransaction ->
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.commit()
+
+
+        }
+    }
+
 }
