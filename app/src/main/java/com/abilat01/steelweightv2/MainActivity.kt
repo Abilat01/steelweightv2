6398ef1ef0f036f57cd54809f923c6fe9e0baab3
Dexp <Abilat01@yandex.ru>
2020-08-09 15:03:32 +0300

    New

--- app/src/main/java/com/abilat01/steelweightv2/MainActivity.kt
+++ app/src/main/java/com/abilat01/steelweightv2/MainActivity.kt
@@ -2,8 +2,13 @@ package com.abilat01.steelweightv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.abilat01.steelweightv2.fragments.ExercisesFragment
+import com.abilat01.steelweightv2.fragments.InfoFragment
+import com.abilat01.steelweightv2.fragments.ProgramFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,39 +16,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Загрузка первых фрагментов
-        loadFragment(ExploreFragment())
 
-        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
-            when {
-                menuItem.itemId == R.id.navigationExplore -> {
-                    loadFragment(ExploreFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
-                menuItem.itemId == R.id.navigationDashboard -> {
-                    loadFragment(DashboardFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
-                menuItem.itemId == R.id.navigationProfile ->{
-                    loadFragment(ProfaleFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
+        //Загрузка главного фрагмента(В первую очередь)
+        loadFragment(ProgramFragment())
 
+        bottomNavigationView.setOnNavigationItemSelectedListener { MenuItem ->
+            when (MenuItem.itemId) {
+                R.id.menu_programs -> {
+                    loadFragment(ProgramFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_exercises -> {
+                    loadFragment(ExercisesFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_info -> {
+                    loadFragment(InfoFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
                 else -> {
                     return@setOnNavigationItemSelectedListener false
-
                 }
             }
-
         }
+
     }
 
     private fun loadFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().also { fragmentTransaction ->
-            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.replace(R.id.fragment_container, fragment)
             fragmentTransaction.commit()
-
-
         }
     }
 
--- app/src/main/java/com/abilat01/steelweightv2/fragments/ExercisesFragment.kt
+++ app/src/main/java/com/abilat01/steelweightv2/fragments/ExercisesFragment.kt
@@ -1,10 +1,11 @@
-package com.abilat01.steelweightv2
+package com.abilat01.steelweightv2.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.abilat01.steelweightv2.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DashboardFragment.newInstance] factory method to
+ * Use the [ExercisesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DashboardFragment : Fragment() {
+class ExercisesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return inflater.inflate(R.layout.fragment_exercises, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class DashboardFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DashboardFragment.
+         * @return A new instance of fragment ExercisesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DashboardFragment().apply {
+            ExercisesFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
