1d85dd3dd01ce5f817e0ce0af2055dc8f7b03adc
Abilat01 <abilat01@yandex.ru>
2020-08-10 16:59:05 +0300

    dez

--- app/src/main/java/com/abilat01/steelweightv2/fragments/ExercisesFragment.kt
+++ app/src/main/java/com/abilat01/steelweightv2/fragments/ExercisesFragment.kt
@@ -8,11 +8,25 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.abilat01.steelweightv2.MyAdapter
 import com.abilat01.steelweightv2.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main_list.*
 
 
 class ExercisesFragment : Fragment(R.layout.fragment_exercises) {
 
-private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mRecyclerView: RecyclerView
     private lateinit var mMyAdapter: MyAdapter
 
+    override fun onResume() {
+        super.onResume()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        mRecyclerView = main_list_recycle_view
+        mMyAdapter = MyAdapter()
+        //И хули дальше?)
+
+        mRecyclerView.adapter = mMyAdapter
+    }
 }
